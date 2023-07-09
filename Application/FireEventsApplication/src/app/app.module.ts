1c157af87a6d9bf0d3ce24209536cc3418828254
Felippe Delesporte <felippedelesporte@hotmail.com>
2023-07-09 16:32:52 -0300

    Interpolation, directives and binding

--- Application/FireEventsApplication/src/app/app.module.ts
+++ Application/FireEventsApplication/src/app/app.module.ts
@@ -1,26 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonModule} from 'primeng/button';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
-import {HttpClientModule} from '@angular/common/http'
+import { NavComponent } from './nav/nav.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
-    PalestrantesComponent
+    PalestrantesComponent,
+    NavComponent
   ],
-  imports: [
+  imports: [  
+    AppRoutingModule,
+    HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
-    ButtonModule,
-    AppRoutingModule,
-    HttpClientModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Application/FireEventsApplication/src/app/eventos/eventos.component.ts
+++ Application/FireEventsApplication/src/app/eventos/eventos.component.ts
@@ -4,10 +4,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.scss']
+  styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent {
-  public eventos: any;
+  public events: any = [];
+  public filteredEventsList: any = [];
+  widthImg: number = 130;
+  marginImg: number = 1;
+  showImg: boolean = true;
+  private filterValue: string = '';
 
   constructor(private http: HttpClient) {}
 
@@ -15,10 +20,37 @@ export class EventosComponent {
     this.getEventos();
   }
 
-  public getEventos(): void{
+  public getEventos(): void {
     this.http.get('https://localhost:7104/api/eventos').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
+      (response) => {
+        this.events = response;
+        this.filteredEventsList = this.events;
+      },
+      (error) => console.log(error)
+    );
+  }
+
+  showImageToggle() {
+    this.showImg = !this.showImg;
+  }
+
+  public get listFilter(): string {
+    return this.filterValue;
+  }
+
+  public set listFilter(value: string) {
+    this.filterValue = value;
+    this.filteredEventsList = this.listFilter
+      ? this.filteringList(this.listFilter)
+      : this.events;
+  }
+
+  filteringList(listFilter: string): any {
+    listFilter = listFilter.toLocaleLowerCase();
+    return this.events.filter(
+      (evento: { tema: string; endereco: string }) =>
+        evento.tema.toLocaleLowerCase().indexOf(listFilter) !== -1 ||
+        evento.endereco.toLocaleLowerCase().indexOf(listFilter) !== -1
     );
   }
 }
